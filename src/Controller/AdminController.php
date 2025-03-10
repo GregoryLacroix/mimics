5914d55d32334a6c2dfe1dbadb512248fcb454ff
gregory LACROIX <gregorylacroix78@gmail.com>
2025-03-10 16:56:45 +0100

    dev backoffice category crud + access admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryFormType;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -21,10 +26,90 @@ final class AdminController extends AbstractController
     }
 
     #[Route('/admin/category', name: 'app_admin_category')]
-    public function adminCategory(): Response
+    public function adminCategory(Request $request, EntityManagerInterface $entityManager, CategoryRepository $repoCategory): Response
     {
-        return $this->render('admin/category.html.twig', []);
+        $category = new Category;
+
+        $form = $this->createForm(CategoryFormType::class, $category);
+
+        // $category->setTitle($_POST['title']);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $category->setCreatedAt(new \DateTimeImmutable());
+
+            // $stmt->prepare("INSERT INTO category VALUES (:title)")
+            // $stmt->bindValue(':title', $category->getTitle(), PDO::PARAM_STR);
+            $entityManager->persist($category);
+
+            // $stmt->execute();
+            $entityManager->flush();
+
+            // Message utilisateur stockés en session
+            $this->addFlash('success', "La catégorie a été enregistrée.");
+
+            return $this->redirectToRoute('app_admin_category');
+        }
+
+        /*
+            $data = $connect_db->query("SELECT * FROM category");
+            $dbCategory = $data->fetchAll(PDO::FETCH_ASSOC);
+
+            Un classe Repository contient des méthodes permettant uniquement d'executer des requêtes de sélections (SELECT) en BDD  (find($id), findAll(), findBy(), findOneBy())
+        */
+        $dbCategory = $repoCategory->findAll();
+        dump($dbCategory);
+
+        return $this->render('admin/category.html.twig', [
+            'categoryForm' => $form,
+            'dbCategory' => $dbCategory
+        ]);
     }
+
+    #[Route('/admin/category/update/{id}', name: 'app_admin_category_update')]
+    public function adminCategoryUpdate($id, Category $category, Request $request, EntityManagerInterface $entityManager, CategoryRepository $repoCategory): Response
+    {
+        // dump($category);
+
+        // SELECT * FROM category WHERE id = $id; // 1
+        // + fetch(PDO::FETCH_ASSOC)
+        $category = $repoCategory->find($id);
+        // dump($id);
+        // dump($category);
+
+        $form = $this->createForm(CategoryFormType::class, $category);
+
+        // $category->setTitle($_POST['title'])
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // UPDATE category SET title = $category->geTitle(), description = $category->getDescription WHERE id = $id
+            $entityManager->persist($category);
+            $entityManager->flush();
+
+            dump($category->getTitle());
+            $categoryTitle = $category->getTitle();
+
+            $this->addFlash('success', "La catégorie <strong class='text-white'>$categoryTitle</strong> a été modifiée.");
+
+            return $this->redirectToRoute('app_admin_category');
+        }
+
+        $dbCategory = $repoCategory->findAll();
+
+        return $this->render('admin/category.html.twig', [
+            'categoryForm' => $form,
+            'dbCategory' => $dbCategory
+        ]);
+    }
+
+    #[Route('/admin/category/remove/{id}', name: 'app_admin_category_remove')]
+    public function adminCategoryRemove($id, EntityManagerInterface $entityManager, CategoryRepository $repoCategory) {
+        
+    }
+
 
     #[Route('/admin/orders', name: 'app_admin_orders')]
     public function adminOrders(): Response
